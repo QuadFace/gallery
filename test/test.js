1131b6a7bcc9bad1359556277427958995726fd2
Pranay Prakash <pranay.gp@gmail.com>
2015-11-01 14:57:00 -0600

    Added more unit tests

--- test/test.js
+++ test/test.js
@@ -5,4 +5,13 @@ describe('getImageList()', function(){
 	it('should be an array',function(){
 		index.getImageList().should.be.an.instanceof(Array);
 	});
+	it('should\'nt be empty', function(){
+		index.getImageList().length.should.not.equal(0);
+	});
+	it('should only contain images',function(){
+		var images = index.getImageList();
+		for(index in images){
+			images[index].match(/\.(JPG|jpg|JPEG|jpeg|PNG|png|GIF|gif)/).should.not.equal(null);
+		}
+	});
 });
